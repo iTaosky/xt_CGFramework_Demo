637682576931ad1df2a75ea4203d14fd8891e91b
itaosky <taosky@GitHub.com>
2021-01-27 15:25:53 +0800

    push

---
+++ CGFrameworkExample/CGFrameworkExample.framework/Headers/CGFrameworkExample.h
@@ -0,0 +1,18 @@
+//
+//  CGFrameworkExample.h
+//  CGFrameworkExample
+//
+//  Created by Thomas on 2021/1/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CGFrameworkExample.
+FOUNDATION_EXPORT double CGFrameworkExampleVersionNumber;
+
+//! Project version string for CGFrameworkExample.
+FOUNDATION_EXPORT const unsigned char CGFrameworkExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGFrameworkExample/PublicHeader.h>
+
+
